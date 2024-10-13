e9bd8b4977725c55a456c375b173b33714282634
ckjolhede <ckjolhede@gmail.com>
2024-10-12 23:12:45 -0600

    handroll joins tables

--- server/models.py
+++ server/models.py
@@ -6,21 +6,58 @@ from sqlalchemy.orm import validates
 from config import bcrypt, db
 from sqlalchemy.ext.hybrid import hybrid_property
 
-# Models go here!
+###############################################################################
+#studentcourses = db.Table('student_courses', 
+#    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
+#    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True)                       
+#)
+
+#studentassignments = db.Table('student_assignments',
+#    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
+#    db.Column('assignment_id', db.Integer, db.ForeignKey('assignments.id'), primary_key=True)
+#)
+class StudentAssignment(db.Model, SerializerMixin):
+    __tablename__ = 'student_assignments'
+    id = db.Column(db.Integer, primary_key=True)
+    points_earned = db.Column(db.Integer, nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    assignment_id = db.Column(db.Integer, db.ForeignKey('assignments.id'), nullable=False)
+    
+    serializer_rules = ('-assignments.student_assignments', '-students.student_assignments')
+    
+    def __repr__(self):
+        return f'<StudentAssignment {self.id} {self.points_earned} {self.assignment_id}>'
+class StudentCourse(db.Model, SerializerMixin):
+    __tablename__ = 'student_courses'
+    id = db.Column(db.Integer, primary_key=True)
+
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    
+    serializer_rules = ('-students.student_courses', '-courses.student_courses')
+
+    def __repr__(self):
+        return f'<StudentCourse {self.student_id} {self.course_id}>'
+
 class Course(db.Model, SerializerMixin):
     __tablename__ = 'courses'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    #one to manys
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
-    assignments = db.relationship('Assignment', backref='courses', lazy=True)
-    
+    assignments = db.relationship('Assignment', backref='courses')
+    #many to many relationship
+    students = db.relationship('Student', secondary="student_courses", back_populates='courses') 
+
+    teachers = db.relationship('Teacher', back_populates='courses')
     serializer_rules = ('-teacher.courses', '-teacher.assignments', '-assignments.courses', '-assignments.teachers')
 
     def __repr__(self):
         return f'<Course {self.name}>'
     
-
+###############################################################################
 class Student(db.Model, SerializerMixin):
     __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True)
@@ -28,94 +65,110 @@ class Student(db.Model, SerializerMixin):
     last_name = db.Column(db.String(100), nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    student_assignments = db.relationship('StudentAssignment', back_populates='students', lazy=True)
-    student_courses = db.relationship('Course', secondary='students_courses', back_populates='students', lazy=True)
+    
+    assignments = db.relationship('Assignment', secondary='student_assignments', back_populates='students') 
+    courses = db.relationship('Course', secondary="student_courses", back_populates='students')
 
-    serializer_rules = ('-student_assignments.students', '-student_assignments.assignments', '-student_courses.students', '-student_courses.courses')
+    serializer_rules = ('-student_assignments.students', '-student_courses.students')
     @validates('password')
     def validate_password(self, key, password):
         if not password:
             raise ValueError('Password cannot be left blank')
         return password
-    
     @hybrid_property
     def password_hash(self):
         return self._password_hash
-    
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(password.encode("utf8"))
         self._password_hash = password_hash.decode("utf8")
-
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode("utf8"))
     def __repr__(self):
         return f'<Student {self.first_name} {self.last_name}>'
 
-    
+###############################################################################   
 class Teacher(db.Model, SerializerMixin):
     __tablename__ = 'teachers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    
     courses = db.relationship('Course', back_populates='teachers', lazy=True)
-    assignments = db.relationship('Assignment', back_populates='teachers', lazy=True)
-    
-    serializer_rules = ('-courses.teachers', '-courses.assignments', '-assignments.courses', '-assignments.teachers')
-    
 
+    serializer_rules = ('-courses.teachers')
+    
     @validates('password')
     def validate_password(self, key, password):
         if not password:
             raise ValueError('Password cannot be left blank')
         return password
-    
     @hybrid_property
     def password_hash(self):
         return self._password_hash
-    
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(password.encode("utf8"))
         self._password_hash = password_hash.decode("utf8")
-
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode("utf8"))
+    
     def __repr__(self):
         return f'<Teacher {self.name}>'
-    
+
+################################################################################    
 class Assignment(db.Model, SerializerMixin):
     __tablename__ = 'assignments'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     points_possible = db.Column(db.Integer, nullable=True)
-    description = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String, nullable=True)
+    #one to many
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
-    student_assigments = db.relationship('StudentAssignment', back_populates='assignments', lazy=True)
+    #many to many
+    students = db.relationship('Student', secondary=studentassignments, back_populates='assignments')
+
     def __repr__(self):
-        return f'<Assignment {self.name}>'
+        return f'<Assignment: {self.name} | Points Possible: {self.points_possible} | Description:{self.description}>'
     
-class StudentAssignment(db.Model, SerializerMixin):
-    __tablename__ = 'student_assignments'
-    id = db.Column(db.Integer, primary_key=True)
-    points_earned = db.Column(db.Integer, nullable=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
-    assignment_id = db.Column(db.Integer, db.ForeignKey('assignments.id'), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
-    UniqueConstraint('student_id', 'assignment_id', name='student_assignments')
+    serializer_rules = ('-course.assignments', '-course.teachers', '-student_assigments.assignments', '-student_assigments.students')
+
+################################################################################    
+#class StudentAssignment(db.Model, SerializerMixin):
+#    __tablename__ = 'student_assignments'
+#    id = db.Column(db.Integer, primary_key=True)
+#    points_earned = db.Column(db.Integer, nullable=True)
+#    completed = db.Column(db.Boolean, default=False)
     
-    def __repr__(self):
-        return f'<StudentAssignment {self.id} {self.points_earned} {self.assignment_id}>'
+#    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+#    assignment_id = db.Column(db.Integer, db.ForeignKey('assignments.id'), nullable=False)
     
-class StudentCourse(db.Model, SerializerMixin):
-    __tablename__ = 'student_courses'
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
-    UniqueConstraint('student_id', 'course_id', name='student_courses')
+#    student = db.relationship('Student', back_populates='student_assignments')
+#    assignment = db.relationship('Assignment', back_populates='student_assigments')
     
-    def __repr__(self):
-        return f'<StudentCourse {self.student_id} {self.course_id}>'
+#    serializer_rules = ('-assignments.student_assignments', '-students.student_assignments')
+    
+#    def __repr__(self):
+#        return f'<StudentAssignment {self.id} {self.points_earned} {self.assignment_id}>'
+
+################################################################################ 
+
+
+
+#class StudentCourse(db.Model, SerializerMixin):
+#    __tablename__ = 'student_courses'
+#    id = db.Column(db.Integer, primary_key=True)
+    
+#    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+#    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    
+#    student = db.relationship('Student', back_populates='student_courses')
+#    course = db.relationship('Course', back_populates='student_courses')
+    
+#    serializer_rules = ('-students.student_courses', '-courses.student_courses')
+    
+#    def __repr__(self):
+#        return f'<StudentCourse {self.student_id} {self.course_id}>'
+    
     
--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,139 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
+from random import randint, sample, choice as rc
 
 # Remote library imports
 from faker import Faker
+import ipdb
 
 # Local imports
 from app import app
-from models import db
+from models import db, Teacher, Student, Course, Assignment, studentassignments, studentcourses
+
+def create_teachers():
+    teachers = []
+    for _ in range(3):
+        teacher = Teacher(
+            name=fake.last_name(),
+            email=fake.email(), 
+            _password_hash="password",
+            )
+        teachers.append(teacher)
+    return teachers
+
+def create_courses(teachers):
+    courses = []
+    for _ in range(4):
+        teacher_id = rc([teacher.id for teacher in teachers])
+        description = rc(['Biology', 'Chemistry', 'A&P', 'Physics'])
+        course = Course(
+            description=description,
+            teacher_id=teacher_id)
+        courses.append(course)
+    return courses
+        
+def create_students():
+    students = []
+    for _ in range(30):
+        student = Student(
+            first_name=fake.first_name(), last_name=fake.last_name(),
+            email=fake.email(), 
+            _password_hash="password",
+            )
+        students.append(student)
+    return students
+        
+def create_assignments(courses):
+    assignments = []
+    course_ids = [course.id for course in courses]
+    for _ in range(100):
+        #ipdb.set_trace()
+        course_id = rc(course_ids)
+        points_possible = rc([20, 50, 100])
+        description = rc(['fill in the blanks with the correct word', 'write a brief summary of the article', 'select the correct answer for each questions'])
+        assignment = Assignment(
+            name=fake.company(),
+            description=description,
+            points_possible=points_possible,
+            course_id=course_id,
+        )
+        assignments.append(assignment)
+    return assignments
+        
+def create_student_assignments(students, assignments):
+    st_assignments = []
+    for _ in range(100):
+        student_id = rc([student.id for student in students])
+        assignment_id = rc([assignment.id for assignment in assignments])
+        student_assignment = studentassignments(
+            student_id=student_id,
+            assignment_id=assignment_id,
+        )
+        st_assignments.append(student_assignment)
+    return st_assignments
+
+def create_student_courses(students, courses):
+    st_courses = []
+    for _ in range(90):
+        student_id = rc([student.id for student in students])
+        course_id = rc([course.id for course in courses])
+        student_course = studentcourses(
+            student_id=student_id,
+            course_id=course_id
+        )
+        st_courses.append(student_course)
+    return st_courses
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        
+        print("Clearing Tables...")
+        Teacher.query.delete()
+        Course.query.delete()
+        Student.query.delete()
+        Assignment.query.delete()
+        print("Tables cleared!")
+        
         print("Starting seed...")
         # Seed code goes here!
+        print("Seeding Teachers...")
+        teachers = create_teachers()
+        db.session.add_all(teachers)
+        db.session.commit()  
+        print("Teachers seeded!")  
+            
+        print("Seeding Courses...")
+        courses = create_courses(teachers)
+        db.session.add_all(courses)
+        db.session.commit()   
+        print("Courses seeded!")
+        
+        print("Seeding Students...")
+        students = create_students()
+        db.session.add_all(students)
+        db.session.commit()
+        print("Students seeded!")
+
+
+        
+
+        print("Seeding Assignments...")
+        assignments = create_assignments(courses)
+        db.session.add_all(assignments)
+        db.session.commit()  
+        print("Assignments seeded!")      
+
+        print("Seeding Student Assignments...")
+        student_assignments = create_student_assignments(students, assignments)
+        db.session.add_all(student_assignments)
+        db.session.commit()  
+        print("Student Assignments seeded!")      
+
+        print("Seeding Student Courses...")
+        student_courses = create_student_courses(students, courses)
+        db.session.add_all(student_courses)
+        db.session.commit()
+        
+        print("Done!")
